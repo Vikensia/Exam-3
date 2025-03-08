9c8e86a173a5e6407a9ec715c6d65a63259d4de0
Viktoriia Vasyliuk <viktoria.10.05q@gmail.com>
2025-03-08 16:43:43 +0200

    slider realization

--- assets/js/dist/gallery.dev.js
+++ assets/js/dist/gallery.dev.js
@@ -4,19 +4,17 @@ document.addEventListener("DOMContentLoaded", function () {
   var images = document.querySelectorAll(".photo");
   var positions = ["photo-1", "photo-2", "photo-3"];
   setInterval(function () {
-    // Получаем текущие классы элементов
     var currentClasses = Array.from(images).map(function (img) {
       return positions.find(function (pos) {
         return img.classList.contains(pos);
       });
-    }); // Меняем классы местами по кругу
-
+    });
     images.forEach(function (img, index) {
       var _img$classList;
 
       (_img$classList = img.classList).remove.apply(_img$classList, positions);
 
-      img.classList.add(currentClasses[(index + 2) % positions.length]); // Смещаем на один шаг назад
+      img.classList.add(currentClasses[(index + 2) % positions.length]);
     });
-  }, 5000); // Менять классы каждые 2 секунды
+  }, 5000);
 });
--- assets/js/gallery.js
+++ assets/js/gallery.js
@@ -3,16 +3,14 @@ document.addEventListener("DOMContentLoaded", () => {
     const positions = ["photo-1", "photo-2", "photo-3"];
 
     setInterval(() => {
-        // Получаем текущие классы элементов
         let currentClasses = Array.from(images).map(img =>
             positions.find(pos => img.classList.contains(pos))
         );
 
-        // Меняем классы местами по кругу
         images.forEach((img, index) => {
             img.classList.remove(...positions);
-            img.classList.add(currentClasses[(index + 2) % positions.length]); // Смещаем на один шаг назад
+            img.classList.add(currentClasses[(index + 2) % positions.length]);
         });
 
-    }, 5000); // Менять классы каждые 2 секунды
+    }, 5000);
 });
